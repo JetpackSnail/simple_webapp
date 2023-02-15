97766169b10748946817c969517f3b4bd5f2dbd5
gohhaoming <50420411+JetpackSnail@users.noreply.github.com>
2023-02-16 01:20:01 +0800

    Change filename for EBS deployment

--- workshop01/application.py
+++ workshop01/application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from helpers import get_rand_quote, get_url
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html', quote=get_rand_quote(), repo_url=get_url())
