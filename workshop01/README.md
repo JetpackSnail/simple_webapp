1a892dd4f7e92f2fa1153e3fb4bb40f5357ebbc4
gohhaoming <50420411+JetpackSnail@users.noreply.github.com>
2023-02-15 23:23:45 +0800

    Initial code dump for workshop01

---
+++ workshop01/README.md
@@ -0,0 +1,17 @@
+##  Containers for Deployment and Scaling Pre-Course Assignment
+
+### How to run locally
+
+1) Install prequisites in virutal environment
+
+```
+virtualenv .env/workshop01
+source .env/workshop01/bin/activate
+pip3 install -r requirements.txt
+```
+
+2) Launch flask app
+
+```
+flask run
+```
---
+++ workshop01/helpers.py
@@ -0,0 +1,20 @@
+import random
+
+quotes = [
+    "Logic will get you from A to B. Imagination will take you everywhere.",
+    "There are 10 kinds of people. Those who know binary and those who don't.",
+    "There are two ways of constructing a software design. \
+        One way is to make it so simple that there are obviously no deficiencies and the other is to make it so complicated that there are no obvious deficiencies.",
+    "It's not that I'm so smart, it's just that I stay with problems longer.",
+    "It is pitch dark. You are likely to be eaten by a grue."
+]
+
+def get_rand_quote():
+    ''' Returns a random quote from list
+    '''
+    return random.choice(quotes)
+
+def get_url():
+    ''' Returns link to repository
+    '''
+    return "https://www.<>.com"
